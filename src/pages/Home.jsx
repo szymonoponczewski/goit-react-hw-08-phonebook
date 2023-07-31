82e6ac5254c58f677930ab255f0cb6d0190fa7ab
Szymon Oponczewski <szymon_o@mac.com>
2023-07-31 22:58:17 +0200

    index update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,42 @@
 import React from "react";
+import { FaPhoneSquareAlt } from "react-icons/fa";
 
 const Home = () => (
-  <div>
-    <p>Welcome to Phonebook!</p>
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      overflow: "hidden",
+    }}
+  >
+    <style>
+      {`
+        @keyframes fadeIn {
+          0% {
+            opacity: 0;
+          }
+          50% {
+            opacity: 1;
+          }
+          100% {
+            opacity: 0;
+          }
+        }
+      `}
+    </style>
+    <p
+      style={{
+        fontSize: "60px",
+        fontWeight: "bold",
+        color: "#fff",
+        animation: "fadeIn 4s ease infinite",
+      }}
+    >
+      Welcome to <FaPhoneSquareAlt style={{ verticalAlign: "middle" }} />{" "}
+      Phonebook!
+    </p>
   </div>
 );
 
