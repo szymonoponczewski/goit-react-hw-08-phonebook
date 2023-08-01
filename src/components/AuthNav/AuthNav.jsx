708442f576e2adb0f986c3dab852e1f8ec3ad2d4
Szymon Oponczewski <szymon_o@mac.com>
2023-08-01 22:16:58 +0200

    NavLink to StyledLink change in AuthNav

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,15 +1,28 @@
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 import css from "./AuthNav.module.css";
+
+const StyledLink = styled(NavLink)`
+  color: #fff;
+  font-weight: 600;
+  text-decoration: none;
+  font-size: 19px;
+  transition: transform 0.35s;
+
+  &.active {
+    color: orange;
+  }
+`;
 
 export const AuthNav = () => {
   return (
     <div className={css.wrapper}>
-      <NavLink to="/register" className={css.element}>
+      <StyledLink to="/register" className={css.element} exact>
         Register
-      </NavLink>
-      <NavLink to="/login" className={css.element}>
+      </StyledLink>
+      <StyledLink to="/login" className={css.element} exact>
         Sign in
-      </NavLink>
+      </StyledLink>
     </div>
   );
 };
